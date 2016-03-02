7bf24070521a14abd7b5fc0eac20d23941901601
Anthony RAZAFINDRAMANANA <a.razafindramanana@estia.local>
2016-03-02 16:25:13 +0000

    Change the design of the notification for the server

--- src/notifications/alert.js
+++ src/notifications/alert.js
@@ -64,20 +64,16 @@ function notification(message, type) {
 
 function progressSpinner(message) {
   /**
+    # TO DO place the value for the spinner
       <div class="snackbar">
       <table>
         <tr>
           <td>
-            <i class="fa fa-10px fa-info-circle"></i>
+            <span class="mdl-spinner mdl-js-spinner is-active"></span>
+            <tspan id="spValue">100%</tspan>
           </td>
           <td>
             <div class="snackbar_text">File is uploading</div>
-          </td>
-          <td>
-            <span class="mdl-spinner mdl-js-spinner is-active"></span>
-          </td>
-          <td>
-            <tspan id="spValue"></tspan>
           </td>
         </tr>
       </table>
@@ -91,26 +87,18 @@ function progressSpinner(message) {
   const tr = document.createElement('tr');
 
   const td1 = document.createElement('td');
-
-  const icon = document.createElement('i');
-  icon.className = 'fa fa-10px fa-info-circle';
-  td1.appendChild(icon);
-
+  const spinner = document.createElement('span');
+  spinner.className = 'mdl-spinner mdl-js-spinner is-active';
+  const valueSpinner = document.createElement('tspan');
+  valueSpinner.id = 'spValue';
+  td1.appendChild(spinner);
+  td1.appendChild(valueSpinner);
+  
   const td2 = document.createElement('td');
   const snackbarText = document.createElement('div');
   snackbarText.className = 'snackbar_text';
   snackbarText.innerHTML = message;
   td2.appendChild(snackbarText);
-
-  const td3 = document.createElement('td');
-  const spinner = document.createElement('span');
-  spinner.className = 'mdl-spinner mdl-js-spinner is-active';
-  td3.appendChild(spinner);
-
-  const td4 = document.createElement('td');
-  const valueSpinner = document.createElement('tspan');
-  valueSpinner.id = 'spValue';
-  td4.appendChild(valueSpinner);
 
   const snackbarNotification = document.createElement('div');
   snackbarNotification.className = 'snackbar';
@@ -120,19 +108,11 @@ function progressSpinner(message) {
 
   tr.appendChild(td1);
   tr.appendChild(td2);
-  tr.appendChild(td3);
-  tr.appendChild(td4);
   table.appendChild(tr);
   snackbarNotification.appendChild(table);
   componentHandler.upgradeElement(spinner);
   layoutNotification.appendChild(snackbarNotification);
   layoutNotification.appendChild(separator);
-
-
-  setTimeout(function () {
-    layoutNotification.removeChild(snackbarNotification);
-    layoutNotification.removeChild(separator);
-  }, 5000);
 
 }
 
